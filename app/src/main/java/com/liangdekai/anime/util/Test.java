34d142a6b599392e76c07e262c971d7862fed57c
kid <kid0916@163.com>
2016-08-14 23:43:36 +0800

    l

--- app/src/main/java/com/liangdekai/anime/util/Test.java
+++ app/src/main/java/com/liangdekai/anime/util/Test.java
@@ -13,7 +13,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
